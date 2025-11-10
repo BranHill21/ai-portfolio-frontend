d49e5af014750058e0417ead94cc176f131e9a5f
BranHill21 <brandon.hill2@verizon.net>
2025-11-09 21:52:51 -0600

    Adding test page for spring boot backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import Insights from "./pages/Insights";
+import TestPage from "./pages/TestPage";
 
 function App() {
   // const [user, setUser] = useState(null);
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<LoginPage onLogin={setUser} />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/insights" element={<Insights />} />
+        <Route path="/testpage" element={<TestPage />} />
       </Routes>
     </Router>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,8 @@ const NavBar = () => {
     <nav style={{ padding: "1rem", background: "#20232a", color: "#61dafb" }}>
       <Link style={{ marginRight: "1rem", color: "#61dafb" }} to="/">Login</Link>
       <Link style={{ marginRight: "1rem", color: "#61dafb" }} to="/dashboard">Dashboard</Link>
-      <Link style={{ color: "#61dafb" }} to="/insights">Insights</Link>
+      <Link style={{ marginRight: "1rem", color: "#61dafb" }} to="/insights">Insights</Link>
+      <Link style={{ marginRight: "1rem", color: "#61dafb" }} to="/testpage">TestPage</Link>
     </nav>
   );
 };
