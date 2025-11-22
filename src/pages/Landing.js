7f5bc1ccbb896731f7de28ce50acb3f6743c48ae
BranHill21 <brandon.hill2@verizon.net>
2025-11-21 18:33:53 -0600

    Threads landing page implemented

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,50 @@
+import Threads from "./Threads";
+
 const Landing = () => {
-    return (
-        <div>
-            <h1>Welcome.</h1>
-            <p>Lots of big changes are coming...</p>
-        </div>
-    );
-}
+  return (
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      {/* ---------------- BACKGROUND THREADS ---------------- */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: -1,               // 👈 Keeps it behind everything
+          pointerEvents: "none",    // 👈 Prevents it from blocking clicks
+        }}
+      >
+        <Threads
+          amplitude={1}
+          distance={0.2}
+          enableMouseInteraction={false}
+          color={[0, 1, 0]}
+        />
+      </div>
+
+      {/* ---------------- FOREGROUND CONTENT ---------------- */}
+      <div
+        style={{
+          position: "relative",
+          zIndex: 1,
+          textAlign: "center",
+          paddingTop: "20vh",
+          color: "black",
+        }}
+      >
+        <h1>Stockfolioai</h1>
+        <p>Wall street level analytics brought to your computer</p>
+
+        <button style={{ padding: "10px 20px", marginTop: "20px" }}>
+          Login
+        </button>
+        <button style={{ padding: "10px 20px", marginTop: "20px" }}>
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
