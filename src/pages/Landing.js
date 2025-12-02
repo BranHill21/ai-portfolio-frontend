6b03bc428f04f82139254653ef8e33f397b3c9ac
BranHill21 <brandon.hill2@verizon.net>
2025-12-01 20:11:27 -0600

    adding ping to landing page on load/useEffect

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,20 @@
 import Threads from "./Threads";
+import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import styles from "./Landing.module.css";
+import API from "../api";
 
 const Landing = () => {
   const navigate = useNavigate();
+
+  const pingAPI = async () => {
+      try {
+        const res = await API.get(`/api/ping`);
+        console.log("Console is awake, status:", res)
+      } catch (err) {
+        console.error("Failed to ping:", err);
+      }
+    };
 
   const goToLogin = (e) => {
     e.preventDefault();
@@ -14,6 +25,10 @@ const Landing = () => {
     e.preventDefault();
     navigate("/register");
   };
+
+  useEffect(() => {
+    pingAPI();
+  }, []);
 
   return (
     <div className={styles.landingWrapper}>
